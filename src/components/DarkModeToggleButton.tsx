c82b53e0e118b23303bafe186522b5b7e5c325b8
Rhino-ty <okko8522@gmail.com>
2023-02-18 17:01:13 +0900

    style: DarkModeToggleButton 컴포넌트의 버튼 디자인 변경
    테일윈드 덕분에 dark:로 쉽게 구현 가능, 다크모드일때는 slate로 가져오자!

--- src/components/DarkModeToggleButton.tsx
+++ src/components/DarkModeToggleButton.tsx
@@ -5,7 +5,9 @@ export default function DarkModeToggleButton() {
 
   return (
     <button
-      className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+      className="inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 
+      bg-gray-100 hover:bg-gray-50 hover:text-orange-500 
+      dark:bg-slate-400 dark:hover:text-yellow-300 dark:hover:bg-slate-700"
       type="button"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
@@ -14,7 +16,7 @@ export default function DarkModeToggleButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="w-6 h-6"
+        className="visible dark:invisible dark:w-0 dark:h-0 w-5 h-5"
       >
         <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
       </svg>
@@ -24,7 +26,7 @@ export default function DarkModeToggleButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="w-6 h-6"
+        className="invisible dark:visible dark:w-5 dark:h-5 w-0 h-0"
       >
         <path
           fillRule="evenodd"
