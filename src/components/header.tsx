548730a618e2f8e38b85e54722f8a381edf16bd9
Rhino-ty <okko8522@gmail.com>
2023-02-19 12:42:24 +0900

    feat: 로고 변경 및 제목 없애기

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import DarkModeToggleButton from "./DarkModeToggleButton";
+import rhino from "../../public/Rhino.jpeg";
 
 export default function Header() {
   return (
@@ -9,19 +11,11 @@ export default function Header() {
           href="/"
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 cursor-pointer"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">Taeyeon-Portfoilo</span>
+          <Image
+            src={rhino}
+            alt="Rhino"
+            className="w-10 h-10 rounded-full flex-shrink-0 object-cover object-center hover:brightness-75"
+          />
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/" className="mr-5 hover:text-gray-900 cursor-pointer">
