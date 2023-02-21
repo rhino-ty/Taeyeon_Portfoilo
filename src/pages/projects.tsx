e3b4922b735c5689d982656deba805730e9940d1
Rhino-ty <okko8522@gmail.com>
2023-02-21 13:11:53 +0900

    feat: 데이터 펫칭을 위한 axios 코드 작성

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import axios from "axios";
 
 export default function projects() {
   return (
@@ -16,7 +17,28 @@ export default function projects() {
   );
 }
 
+// 빌드타임에 호출, 근데 revalidate 옵션있어서 갱신도 된다! 이러면 괜찮음
 export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    url: "https://api.notion.com/v1/databases/database_id/query",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-02-22",
+      "content-type": "application/json",
+      Authorization: "Bearer ",
+    },
+    data: { page_size: 100 },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
   return {
     props: {},
     revalidate: 1,
