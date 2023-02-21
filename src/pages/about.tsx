758b149c65c8957ab62d7cf1bebbc9d721a0cdc9
Rhino-ty <okko8522@gmail.com>
2023-02-21 12:10:23 +0900

    feat: index를 제외한 나머지 페이지에도 Head 적용

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,15 @@
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 export default function projects() {
   return (
     <Layout>
+      <Head>
+        <title>About 윤태연</title>
+        <meta name="description" content="개발자 윤태연에 대해서" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>자기소개</h1>
     </Layout>
   );
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,16 @@
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 export default function projects() {
   return (
     <Layout>
+      <Head>
+        <title>윤태연의 프로젝트</title>
+        <meta name="description" content="윤태연의 프로젝트 리스트입니다." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <h1>프로젝트</h1>
     </Layout>
   );
