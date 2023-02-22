09658e33fda3e94816a3d38ecc544516fb2128e4
Rhino-ty <okko8522@gmail.com>
2023-02-22 19:28:55 +0900

    deploy: 배포는 됐는데 /projects가 안된다..
    null을 붙이니 아예 서버와 통신이 안된다는 걸 알았다.. 왜이러지..??? 데이터가 아예 안온다.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import { useEffect, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import { DATABASE_ID, TOKEN } from "../../config";
 import ProjectItem from "../components/projects/ProjectItem";
 
@@ -43,64 +43,64 @@ export default function Projects({ projects }: ProjectsProps) {
 }
 
 // 각 요청 때마다 호출
-export async function getServerSideProps() {
-  const options = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Notion-Version": "2022-02-22",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    },
-    body: JSON.stringify({
-      sorts: [
-        {
-          property: "이름",
-          direction: "ascending",
-        },
-      ],
-      page_size: 100,
-    }),
-  };
-
-  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
-  const data = await res.json();
-  const projects = await data.results;
-
-  return {
-    props: { projects }, // will be passed to the page component as props
-    // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
-    // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
-  };
-}
-
-// 빌드타임에 호출, 근데 revalidate 옵션있어서 갱신도 된다! 이러면 괜찮음
-// export async function getStaticProps() {
+// export async function getServerSideProps() {
 //   const options = {
 //     method: "POST",
-//     url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
 //     headers: {
-//       accept: "application/json",
+//       Accept: "application/json",
 //       "Notion-Version": "2022-02-22",
-//       "content-type": "application/json",
+//       "Content-Type": "application/json",
 //       Authorization: `Bearer ${TOKEN}`,
 //     },
-//     data: {
-//       page_size: 100,
+//     body: JSON.stringify({
 //       sorts: [
 //         {
 //           property: "이름",
 //           direction: "ascending",
 //         },
 //       ],
-//     },
+//       page_size: 100,
+//     }),
 //   };
 
-//   const response = await axios.request(options);
-//   const projects = response.data.results;
+//   const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
+//   const data = await res.json();
+//   const projects = await data.results;
 
 //   return {
-//     props: { projects },
-//     revalidate: 1,
+//     props: { projects }, // will be passed to the page component as props
+//     // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
+//     // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
 //   };
 // }
+
+// 빌드타임에 호출, 근데 revalidate 옵션있어서 갱신도 된다! 이러면 괜찮음
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-02-22",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    data: {
+      page_size: 100,
+      sorts: [
+        {
+          property: "이름",
+          direction: "ascending",
+        },
+      ],
+    },
+  };
+
+  const response = await axios.request(options);
+  const projects = response.data.results;
+
+  return {
+    props: { projects },
+    revalidate: 1,
+  };
+}
