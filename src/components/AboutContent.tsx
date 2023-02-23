efad58ce14547d3a999b94b4502f09d99e4633a9
Rhino-ty <okko8522@gmail.com>
2023-02-23 23:59:05 +0900

    feat: About Me 중 이름, 이메일 기입 이메일 클릭하면 카피되게 기능 추가!

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,51 +1,76 @@
+import { useState } from "react";
+
 export default function AboutContent() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText("okko8522@naver.com");
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
   return (
     <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
-      <div className="flex flex-col mb-10 lg:items-start items-center">
-        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+      <div className="flex flex-row items-center justify-start mb-10 ">
+        <div className="w-16 h-16 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 shadow-md mr-5 hover:shadow-lg transition-shadow duration-300">
           <svg
+            xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-6 h-6"
             viewBox="0 0 24 24"
+            stroke-width="1.7"
+            stroke="currentColor"
+            className="w-8 h-8"
           >
-            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
           </svg>
         </div>
         <div className="flex-grow">
-          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-          <p className="leading-relaxed text-base">
-            Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub
-            indxgo juice poutine.
-          </p>
-          <a className="mt-3 text-indigo-500 inline-flex items-center">
-            Learn More
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
+          <h1 className="text-gray-900 text-2xl title-font font-medium">윤태연</h1>
+          <p className="leading-relaxed text-base"></p>
         </div>
       </div>
-      <div className="flex flex-col mb-10 lg:items-start items-center">
-        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+
+      <div
+        className="flex flex-row items-center justify-start mb-10 cursor-pointer"
+        onClick={handleCopyClick}
+      >
+        <div className="w-16 h-16 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 shadow-md mr-5 hover:shadow-lg transition-shadow duration-300 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.7"
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+            />
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-gray-900 text-2xl title-font font-medium mb-3">okko8522@naver.com</h2>
+          <p className="leading-relaxed text-base"></p>
+        </div>
+        {isCopied && <span className="text-red-400 ml-2">Copied!</span>}
+      </div>
+      <div className="flex flex-row items-center justify-start mb-10 ">
+        <div className="w-16 h-16 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 shadow-md mr-5 hover:shadow-lg transition-shadow duration-300">
           <svg
             fill="none"
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round"
-            stroke-width="2"
-            className="w-6 h-6"
+            stroke-width="1.7"
+            className="w-8 h-8"
             viewBox="0 0 24 24"
           >
             <circle cx="6" cy="6" r="3"></circle>
@@ -54,62 +79,8 @@ export default function AboutContent() {
           </svg>
         </div>
         <div className="flex-grow">
-          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-          <p className="leading-relaxed text-base">
-            Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub
-            indxgo juice poutine.
-          </p>
-          <a className="mt-3 text-indigo-500 inline-flex items-center">
-            Learn More
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <div className="flex flex-col mb-10 lg:items-start items-center">
-        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
-          >
-            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-            <circle cx="12" cy="7" r="4"></circle>
-          </svg>
-        </div>
-        <div className="flex-grow">
-          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-          <p className="leading-relaxed text-base">
-            Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub
-            indxgo juice poutine.
-          </p>
-          <a className="mt-3 text-indigo-500 inline-flex items-center">
-            Learn More
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
+          <h2 className="text-gray-900 text-2xl title-font font-medium mb-3">The Catalyzer</h2>
+          <p className="leading-relaxed text-base"></p>
         </div>
       </div>
     </div>
