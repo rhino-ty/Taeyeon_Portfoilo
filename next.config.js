123588aacb55d58dbcdde4c2127a21667923c88b
Rhino-ty <okko8522@gmail.com>
2023-03-11 11:26:13 +0900

    config: pakage.json 문제가 아니라 config의 bashPath문제였던거

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     domains: ["www.notion.so", "s3.us-west-2.amazonaws.com", "images.unsplash.com"],
     unoptimized: true,
   },
-  basePath: "/taeyeon-portfoilo",
 };
 
 module.exports = nextConfig;
