3cd5eb86e70a7deb53b7869d1e0f2a15ff14a939
Rhino-ty <okko8522@gmail.com>
2023-05-01 20:29:30 +0900

    feat: SelfIntroductionContent 추가

--- src/pages/selfIntroduction.tsx
+++ src/pages/selfIntroduction.tsx
@@ -10,6 +10,12 @@ export default function SelfIntroduction() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-12 lg:flex-row flex-col items-center">
+          <SelfIntroduction />
+        </div>
+      </section>
     </Layout>
   );
 }
