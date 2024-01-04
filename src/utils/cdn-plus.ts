498b0cbd903f2874d3e90bc9ef7f26b2887fea98
Rhino-ty <okko8522@gmail.com>
2024-01-04 18:34:23 +0900

    refactor: 필요없는 유틸 함수들 삭제

--- src/utils/cdn-plus.ts
+++
@@ -1,8 +0,0 @@
-export default function cdnPlus(fileName: string | undefined) {
-  if (fileName) {
-    return `https://cdn.platpharm.co.kr/${fileName}`;
-  }
-  // https://png-pixel.com/
-  // return 'data:image/pgn;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
-  return '/images/logo/other-color.svg';
-}
