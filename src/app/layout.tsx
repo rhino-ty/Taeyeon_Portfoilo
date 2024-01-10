35ff14eb911197f1f664110873631654b8dc04a4
Rhino-ty <okko8522@gmail.com>
2024-01-10 15:29:11 +0900

    feat: metaData metadataBase 추가 및 icons, twitter 등 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,24 @@ const notoSansKr = Noto_Sans_KR({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.taeyeon-portfolio.site/'),
   title: '윤태연의 포트폴리오',
   description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
-  icons: '/favicon.ico',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
   openGraph: {
     title: '윤태연의 포트폴리오',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
+    images: ['/favicon.ico'],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '윤태연의 포트폴리오',
+    description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
+    images: ['/favicon.ico'],
   },
 };
 
