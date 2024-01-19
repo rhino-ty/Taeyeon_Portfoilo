6580cc0b6e39d1de0458816c5dee98128cdabf32
Rhino-ty <okko8522@gmail.com>
2024-01-19 17:48:45 +0900

    UI kit로 Daisy UI 사용하지 않기로 결정
    사용한 결과 생각보다 커스텀이 어려웠고, 내가 원하는 컴포넌트도 없어서 그냥 제외.
    가장 호환성 좋고 디자인도 내스타일인 shadcn/ui로 사용할 것임

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,14 +12,10 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['light', 'dark', 'cupcake'],
-  },
+  plugins: [],
 };
 export default config;
