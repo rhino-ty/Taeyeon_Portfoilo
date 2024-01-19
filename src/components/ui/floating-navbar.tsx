c007e2eba63f6a3f963e99b3b97bbded98e585c1
Rhino-ty <okko8522@gmail.com>
2024-01-19 18:56:46 +0900

    design: Increas font size in nav bar items

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -49,7 +49,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          'fixed inset-x-0 top-5 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-8 py-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black',
+          'fixed inset-x-0 top-5 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-8 py-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black',
           className,
         )}
       >
@@ -63,7 +63,7 @@ export const FloatingNav = ({
             target={navItem.target ? navItem.target : '_self'}
           >
             <span className='block sm:hidden'>{navItem.icon}</span>
-            <span className='hidden text-sm sm:block'>{navItem.name}</span>
+            <span className='hidden text-base sm:block'>{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
