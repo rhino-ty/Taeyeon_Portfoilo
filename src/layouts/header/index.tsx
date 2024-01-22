67ef64e05a7b78f225110aae9dcb55889f374c45
Rhino-ty <okko8522@gmail.com>
2024-01-22 18:21:20 +0900

    feat: Added a Theme manipulation dropdown in Floating Navbar component.

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,9 +1,20 @@
 'use client';
 import React from 'react';
 import { FloatingNav } from '@/components/ui/floating-navbar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import LucideIcon from '@/components/lucide-icon';
+import { useTheme } from 'next-themes';
 
 export default function HeaderComponent({ className }: { className?: string }) {
-  const navItems = [
+  const { setTheme } = useTheme();
+
+  const navItems: NavItem[] = [
     {
       name: 'Resume',
       link: '/',
@@ -15,6 +26,37 @@ export default function HeaderComponent({ className }: { className?: string }) {
     {
       name: 'Notion',
       link: 'https://rhino-ty.notion.site/c56149845fcd49eda4c1c71b17a1d1f7?pvs=4',
+    },
+    {
+      name: 'Theme',
+      html: (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' size='icon'>
+              <LucideIcon
+                name='Sun'
+                className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
+              />
+              <LucideIcon
+                name='Moon'
+                className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
+              />
+              <span className='sr-only'>Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => setTheme('system')} className='text-base'>
+              System
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('light')} className='text-base'>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('dark')} className='text-base'>
+              Dark
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ),
     },
   ];
   return (
