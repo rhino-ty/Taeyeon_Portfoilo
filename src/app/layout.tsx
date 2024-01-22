2d23fb3a243d5cc4139124452f5eed51fd77152e
Rhino-ty <okko8522@gmail.com>
2024-01-22 18:41:59 +0900

    feat: Created the 'page.tsx' file for the portfolio and added metadata to all 'page.tsx' files.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from 'next';
 import '../styles/globals.css';
 import { ThemeProvider } from '@/utils/theme-provider';
 // import { Noto_Sans_KR } from 'next/font/google'; // 한글 NotoSans를 사용.
@@ -39,28 +38,6 @@ const pretendard = localFont({
     'sans-serif',
   ],
 });
-
-export const metadata: Metadata = {
-  metadataBase: new URL('https://www.taeyeon-portfolio.site/'),
-  title: '윤태연의 포트폴리오',
-  description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: '/favicon.ico',
-  },
-  openGraph: {
-    title: '윤태연의 포트폴리오',
-    description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
-    images: ['/favicon.ico'],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: '윤태연의 포트폴리오',
-    description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
-    images: ['/favicon.ico'],
-  },
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,8 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.taeyeon-portfolio.site/'),
-  title: '윤태연의 이력서',
+  title: '윤태연의 포트폴리오',
   description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
   icons: {
     icon: '/favicon.ico',
@@ -11,18 +10,18 @@ export const metadata: Metadata = {
     apple: '/favicon.ico',
   },
   openGraph: {
-    title: '윤태연의 이력서',
+    title: '윤태연의 포트폴리오',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
     images: ['/favicon.ico'],
   },
   twitter: {
     card: 'summary_large_image',
-    title: '윤태연의 이력서',
+    title: '윤태연의 포트폴리오',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
     images: ['/favicon.ico'],
   },
 };
 
-export default function HomeWithResume() {
-  return <div className='h-[1000px]'>이력서</div>;
+export default function PortfolioPage() {
+  return <>포트폴리오페이지</>;
 }
