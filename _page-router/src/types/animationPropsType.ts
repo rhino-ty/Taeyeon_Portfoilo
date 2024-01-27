4413c456772aa50221b38e254631d185645fa7a8
Rhino-ty <okko8522@gmail.com>
2024-01-28 00:06:41 +0900

    refactor: Delete the old version created with Page Router

--- _page-router/src/types/animationPropsType.ts
+++
@@ -1,7 +0,0 @@
-export interface AnimationProps {
-  animationType: AnimationType;
-  width: number;
-  height: number;
-}
-
-type AnimationType = "fun" | "writing" | "telescope" | "strong" | "default";
