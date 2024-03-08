452326aa0e76e8236d834f93204cfe61ec3363f5
Rhino-ty <okko8522@gmail.com>
2024-03-09 00:39:29 +0900

    feat: Change the title of metadata

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.taeyeon-portfolio.site/'),
-  title: '윤태연의 포트폴리오',
+  title: '윤태연의 결과물',
   description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
   icons: {
     icon: '/favicon.ico',
@@ -10,18 +10,18 @@ export const metadata: Metadata = {
     apple: '/favicon.ico',
   },
   openGraph: {
-    title: '윤태연의 포트폴리오',
+    title: '윤태연의 결과물',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
     images: ['/favicon.ico'],
   },
   twitter: {
     card: 'summary_large_image',
-    title: '윤태연의 포트폴리오',
+    title: '윤태연의 결과물',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
     images: ['/favicon.ico'],
   },
 };
 
 export default function PortfolioPage() {
-  return <>포트폴리오페이지</>;
+  return <></>;
 }
