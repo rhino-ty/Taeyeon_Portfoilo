2370e77867cdf64c56841549f8b00169cdfcf1cb
Rhino-ty <okko8522@gmail.com>
2024-06-12 12:02:20 +0900

    feat: Change metadata title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.taeyeon-portfolio.site/'),
-  title: '윤태연이란 개발자',
+  title: '윤태연 | Front-End',
   description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
   icons: {
     icon: '/favicon.ico',
@@ -13,13 +13,13 @@ export const metadata: Metadata = {
     apple: '/favicon.ico',
   },
   openGraph: {
-    title: '윤태연이란 개발자',
+    title: '윤태연 | Front-End',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
     images: ['/favicon.ico'],
   },
   twitter: {
     card: 'summary_large_image',
-    title: '윤태연이란 개발자',
+    title: '윤태연 | Front-End',
     description: '재미와 개발, 제품을 한 몸에 담고싶은 개발자',
     images: ['/favicon.ico'],
   },
